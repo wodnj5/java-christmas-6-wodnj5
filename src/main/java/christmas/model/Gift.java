a9fdd75dff79321dbc91e7114de5f63ffaea38ef
wodnj5 <jw991211@gmail.com>
2023-11-11 17:37:00 +0900

    feat: 증정 이벤트 기능 추가

---
+++ src/main/java/christmas/model/Gift.java
@@ -0,0 +1,29 @@
+package christmas.model;
+
+import static christmas.model.Menu.CHAMPAGNE;
+
+public class Gift {
+
+    private final Menu menu;
+    private int count;
+
+    public Gift(int totalPrice) {
+        menu = validate(totalPrice);
+    }
+
+    private Menu validate(int totalPrice) {
+        if(totalPrice >= 120_000) {
+            count = 1;
+            return CHAMPAGNE;
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        if(menu == null) {
+            return "없음";
+        }
+        return String.format("%s %d개\n", menu.getName(), count);
+    }
+}
